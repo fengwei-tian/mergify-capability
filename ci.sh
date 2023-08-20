9e8a5c796acb3c12bfda3af445a5453bb30d9bdc
tian.fengwei <tfengwei@rippling.com>
2023-08-20 09:59:49 -0700

    update

---
+++ ci.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# if current branch does not start with gh - then return 0
+if [[ ! $BUILDKITE_BRANCH =~ ^gh.* ]]; then
+  echo "Not a gh-pages branch, skipping deploy"
+  exit 0
+fi
+
+val=$(buildkite-agent meta-data get "pof") || true
+echo "Val: $val"
+
+if [[ $val == "1" ]]; then
+  exit 1
+fi
+
+exit 0
